a29546a1107f79b6d03ef14d12a5f6c7eef2438f
Ulrich HOUNGBO <houngbou@gmail.com>
2024-03-22 17:46:00 +0100

    feat: implement passport jwt-strategy

---
+++ apps/auth/src/strategies/jwt.strategy.ts
@@ -0,0 +1,29 @@
+import { PassportStrategy } from '@nestjs/passport';
+import { Injectable } from '@nestjs/common';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { ConfigService } from '@nestjs/config';
+import { UsersService } from '../users/users.service';
+import { Request } from 'express';
+import { TokenPayload } from '../interfaces/jwt-payload.interfaces';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(
+    configService: ConfigService,
+    private readonly usersService: UsersService,
+  ) {
+    super({
+      jwtFromRequest: ExtractJwt.fromExtractors([
+        (request: Request) => {
+          return request?.cookies?.Authentication;
+        },
+      ]),
+      ignoreExpiration: false,
+      secretOrKey: configService.get('JWT_SECRET'),
+    });
+  }
+
+  async validate({ userId }: TokenPayload) {
+    return this.usersService.getUser({ _id: userId });
+  }
+}
