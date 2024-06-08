da32da7abf29100cb3834a4479f673e879e17a0a
Ulrich-houngbo <uhoungbo@gmail.com>
2024-06-08 22:26:54 +0100

    feat: create common user schema

--- apps/auth/src/auth.controller.ts
+++ apps/auth/src/auth.controller.ts
@@ -1,7 +1,6 @@
 import { Controller, Post, Res, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { CurrentUser } from '@app/common';
-import { UserDocument } from './users/models/user.schema';
+import { CurrentUser, UserDocument } from '@app/common';
 import { Response } from 'express';
 import { LocalAuthGuard } from './guards/local-auth.guard';
 import { MessagePattern, Payload } from '@nestjs/microservices';
